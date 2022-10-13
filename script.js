6f5d56796dcddee5d8ea8d385c2dada6c542d20c
Lucas Foxworthy <40660037+EverWand@users.noreply.github.com>
2022-10-12 18:40:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function signIn(){
+
+}
+
+function logIn(){
+    
+}
+
+function validateUser(){
+    let first = document.getElementById("First Name").value;
+    let last = document.getElementById("Last Name").value;
+    let submit= first+ last;
+
+    alert("Hello, " + submit +".")
+}
